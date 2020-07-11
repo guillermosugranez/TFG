f72a6fe37c85c4c6449f7d13916dcf328fdc5ce7
Guillermo Sugráñez Pérez <guillecor91@gmail.com>
2020-07-11 16:36:28 +0200

    Lección 24. Request

--- Flask/simple_app.py
+++ Flask/simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask  # Flask es una clase del módulo flask
+from flask import request
 
 # -----------------------------------------------------------------------------
 
@@ -7,20 +8,19 @@ from flask import Flask  # Flask es una clase del módulo flask
 # Si ejecutas directamente este scrip, es lo que se va a ejecutar en la consola
 # Si se llama desde otro sitio servirá como utilidad
 app = Flask(__name__)  # Instancia de una aplicación de flask
+# No puede haber dos vistas con el mismo nombre
 
 
 @app.route('/')  # Vista principal. Cuando los usuarios no ponen nada más
-def index():  # No puede haber dos vistas con el mismo nombre
+# Añadida variable name. Por defecto name=Mundo
+# Añadida variable lastname. Por defecto lastname=!!!!
+def index(name='Mundo', lastname='!!!'):
     '''Primera función '''
 
-    return "Hola mundo! Mi primera aplicación de Flask."
-
-
-@app.route('/Guille')  # Vista principal. Cuando los usuarios no ponen nada más
-def guille():  # No puede haber dos vistas con el mismo nombre
-    '''Segunda función '''
-
-    return "Hola soy Aldo!!, que diga... Guille"
+    # Si no hay parámetro name en el navegador, usará el name por defecto
+    name = request.args.get('name', name)  # Busca el parámetro name en la dir.
+    lastname = request.args.get('lastname', lastname)
+    return "Hola {} {}. Mi primera aplicación de Flask.".format(name, lastname)
 
 # Un script en flask se llama aplicación
 # Ejecuta la aplicación de flask.
@@ -33,6 +33,7 @@ def guille():  # No puede haber dos vistas con el mismo nombre
 app.run(debug=True, port=8000, host='0.0.0.0')
 
 # -----------------------------------------------------------------------------
+# Lección 23. Route
 
 # Al arrancar la app tal cual, en el navegador aparece not found
 # No maneja la URL proporcionada, no sabe qué tiene que hacer
@@ -40,3 +41,16 @@ app.run(debug=True, port=8000, host='0.0.0.0')
 #   - A través de vistas y routes
 
 # Vista: Método que despliega contenido en el navegador según URL (Route)
+
+# -----------------------------------------------------------------------------
+# Lección 24. parámetros
+
+# ¿Cómo tomar parámetros de las peticiones http?
+# Una forma común es a través de los query parameters
+# Son los que vienen después del signo de interrogación
+# Formato: ?nombre=Guille&apellido=Sugráñez
+# No se puede hacer una ruta para cada posible conjunto de parámetros
+# Habría que usar una variable global que contenga toda la info de la request
+# request es un objeto global que guarda toda la info de la petición http
+# Como es una variable global, se puede acceder a ella desde donde sea
+# Lo normal es manejarlo dentro de las vistas
